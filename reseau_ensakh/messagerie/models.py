a39d77918ef32708f4f04c2b9edb8d4983f1330f
AJude <tamizepro@gmail.com>
2023-07-18 01:13:03 +0100

    Modification du modèle Message

--- reseau_ensakh/messagerie/models.py
+++ reseau_ensakh/messagerie/models.py
@@ -2,20 +2,23 @@ from django.db import models
 from authentification.models import Utilisateur
 
 class Message(models.Model):
+    # Contenu du message:
+    # peut etre du texte, un message vocal ou tout autre fichier
     texte = models.CharField(max_length=1000, null=True, blank=True)
-    fichier_audio = models.FileField(upload_to='audio/', null=True, blank=True)
-    document = models.FileField(upload_to='documents/', null=True, blank=True)
-    image = models.ImageField(upload_to='images/messagerie', null=True, blank=True)
-    video = models.FileField(upload_to='video/messagerie', null=True, blank=True)
+    fichier = models.FileField(upload_to='fichiers/messsagerie/', null=True, blank=True)
     
-    #Indiquent que le message a été reçu ou vu
+    # Indiquent que le message a été reçu ou vu
     recu = models.BooleanField(default=False)
     vu = models.BooleanField(default=False)
+    
+    # La date de création du message
     date_heure_de_creation = models.DateTimeField()
     
-    #Ce message est la réponse à un autre message ou
+    # Ce message est la réponse à un autre message ou
     # Ce message repond à un autre message
     message = models.OneToOneField('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='repondu_par')
     
+    # Expediteur: celui qui envoi le message
+    # Destinataire: celui qui doit recevoir le message
     expediteur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, related_name='envoye_par')
     destinataire = models.ForeignKey(Utilisateur, on_delete=models.CASCADE, related_name='recu_par')
